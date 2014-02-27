3477e744d51a2e4aaf99c04183d7f5d78f652501
isehgu <hgu@ise.com>
2014-02-27 16:44:09 -0500

    adding tac

---
+++ tac/index.php
@@ -0,0 +1,29 @@
+<?php
+	//require_once "base_function.php";
+	//f_dbConnect();
+?>
+	
+
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+            <title>TAC</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="description" content="Test Automation Controller">
+            <meta name="author" content="Autobots">
+            <!--bootstrap-->
+            <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+            <link href="tac.css" rel="stylesheet">
+
+            <script src="js/jquery-1.11.0.min.js"></script>
+            <script src="js/bootstrap.min.js"></script>
+    </head>	
+    <body>
+				<div id="topbar">
+								<p class="text-center">hello world</p>
+								<button class="" id="btn">Run</button>
+				</div>
+   
+    </body>
+	
+</html>
